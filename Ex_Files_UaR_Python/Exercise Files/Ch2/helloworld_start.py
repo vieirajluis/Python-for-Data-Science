d841353967642ec8fb59728f12253b436cd01a03
joaolsvieira <joaolsvieira@gmail.com>
2018-01-22 19:24:33 -0500

    Edit

--- Ex_Files_UaR_Python/Exercise Files/Ch2/helloworld_start.py
+++ Ex_Files_UaR_Python/Exercise Files/Ch2/helloworld_start.py
@@ -2,3 +2,10 @@
 # Example file for HelloWorld
 # (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
 
+def main():
+   print "Hello World!"
+
+if __name__=="__main__":
+    main()
+
+print "me too"
